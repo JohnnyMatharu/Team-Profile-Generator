47d4db7175ebb8276ab1b9be3c1e84a296e63914
Johnny <jaani.matharu@gmail.com>
2021-04-26 14:21:49 -0400

    added basic instructions

---
+++ index.js
@@ -0,0 +1,84 @@
+//Npm installed (check other stuff needs installed)
+//Inquirer installed 
+//JEst installed
+//application should use Jest (Links to an external site.) for running the unit tests
+//Your challenge is to build a Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+//Instead of readme you will use html and instead of yourself alone, you will make this for team
+//Basic methodology is same as last assingment, you will use constructor parent object model and make inherited objects for common employee charateristics 
+//you’ll also write unit tests for each part of your code and ensure that it passes all of them (JEst technique from Module)
+//Because this application won’t be deployed, you’ll also need to provide a link to a walkthrough video that demonstrates its functionality and all of the tests passing. You’ll need to submit a link to the video and add it to the README of your project.
+//add video to github and provide link in readme just like screenshot
+//There is no starter code for this assignment, start from last week's assignment and make changes to it
+
+
+//Acceptance Criteria
+//GIVEN a command-line application that accepts user input
+//WHEN I am prompted for my team members and their information
+//THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+//WHEN I click on an email address in the HTML
+//THEN my default email program opens and populates the TO field of the email with the address
+
+//HTML <a> tag provides you option to specify an email address to send an email. While using <a> tag as an email tag, you will use mailto: email address along with href attribute. Following is the syntax of using mailto instead of using http. This code will generate the following link which you can use to send email.
+//https://stackoverflow.com/questions/34140776/properly-populate-an-email-using-only-html
+
+
+//WHEN I click on the GitHub username
+//THEN that GitHub profile opens in a new tab
+//WHEN I start the application
+//THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+//WHEN I enter the team manager’s name, employee ID, email address, and office number
+//THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+//WHEN I select the engineer option
+//THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+//WHEN I select the intern option
+//THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+//WHEN I decide to finish building my team
+//THEN I exit the application, and the HTML is generated
+
+//Directory structure
+//__tests__/            // jest tests
+//Employee.test.js
+//Engineer.test.js
+//Intern.test.js
+//Manager.test.js
+//dist/               // rendered output (HTML) and CSS style sheet
+//lib/                // classes
+//src/                // template helper code
+//index.js            // runs the application
+
+
+//The application must have these classes: Employee, Manager, Engineer, and Intern. The tests for these classes (in the _tests_ directory) must all pass.
+
+//In addition to Employee's properties and methods, Manager will also have:
+
+//officeNumber
+
+//getRole() // Overridden to return 'Manager'
+
+//In addition to Employee's properties and methods, Engineer will also have:
+
+//github // GitHub username
+
+//getGithub()
+
+//getRole() // Overridden to return 'Engineer'
+
+//In addition to Employee's properties and methods, Intern will also have:
+
+//school
+
+//getSchool()
+
+//getRole() // Overridden to return 'Intern'
+
+//Finally, although it’s not a requirement, you should consider adding validation to ensure that user input provided is in the proper expected format.
+
+
+//How to Submit the Challenge
+//You are required to submit the following for review:
+
+//A walkthrough video demonstrating the functionality of the application and passing tests.
+
+//A sample HTML file generated using your application.
+
+//The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
