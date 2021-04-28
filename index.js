fef73f49e722f26a8ef1ee9ce8a6f69a725f74e4
Johnny <jaani.matharu@gmail.com>
2021-04-28 15:44:21 -0400

    Fixed the initial inquirer, pending OOP model with constructor functions, classes and inheritence with Jest testing etc., and pending HTML model with CSS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//Npm installed (check other stuff needs installed)
+//Npm fully installed 
 //Inquirer installed 
 //JEst installed
+//Need to adjust modules to gitignore and package.json adjustments etc.
 //application should use Jest (Links to an external site.) for running the unit tests
 //Your challenge is to build a Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
 //Instead of readme you will use html and instead of yourself alone, you will make this for team
@@ -9,6 +10,205 @@
 //Because this application won’t be deployed, you’ll also need to provide a link to a walkthrough video that demonstrates its functionality and all of the tests passing. You’ll need to submit a link to the video and add it to the README of your project.
 //add video to github and provide link in readme just like screenshot
 //There is no starter code for this assignment, start from last week's assignment and make changes to it
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+// this will link the license badge page
+//const licensePage = require('./utils/generateMarkdown.js');
+// test
+
+
+
+// test finish
+// TODO: Create an array of questions for user input
+
+
+const questions = () => {
+
+  inquirer.prompt(
+  [{
+      //THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+      // this needs to be fixed to change title of readme.md -check
+      type: "input",
+      name: "name",
+      message: "What is the name of the manager?"
+    },  
+    
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee ID of the manager?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the email address of the manager?"
+        },
+        {
+            type: "input",
+            name: "number",
+            message: "What is the office number of the manager?"
+          },
+ { 
+     type: "list",
+     name: "designation",
+     message: "Would you like to add an engineer or an intern to finish building the team(Press <enter> to select)",
+     choices: ["Engineer", "Intern", "I am done adding team members"]     
+        //icon will display along with designation         
+     }
+
+     //THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+
+//data from inquirer to function class for each employee (Neil)
+
+// you will link this to main function, and add protype/inheritence for intern and engineer and change display and values
+//THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+          
+//follow structure from previous assgn        
+]).then(data =>  {
+    console.log(data)
+    let jobTitle = data.designation;
+//Here you will insert a parent constructor function and assign values to it from data, link it to html div display so each time you make entry it will get displayed in seperate div
+
+    function options (jobTitle){
+      console.log(jobTitle);      
+      if (jobTitle === "Engineer") 
+        {
+          inquirer.prompt(
+            [{
+                type: "input",
+                name: "name",
+                message: "What is the name of the engineer?"
+              },  
+              
+              {
+                  type: "input",
+                  name: "id",
+                  message: "What is the employee ID of the engineer?"
+                },
+                {
+                  type: "input",
+                  name: "email",
+                  message: "What is the email address of the engineer?"
+                  },
+                  {
+                      type: "input",
+                      name: "github",
+                      message: "What is the Github username of the engineer?"
+                    },
+                    { 
+                      type: "list",
+                      name: "continueTitle",
+                      message: "Would you like to continue to add an engineer or an intern to finish building the team(Press <enter> to select)",
+                      choices: ["Engineer", "Intern", "I am done adding team members"]     
+                         //icon will display along with designation         
+                      }
+
+
+                  ]).then(data =>  {
+                    console.log(data);
+                    jobTitle = data.continueTitle;
+                    //Here you will insert a parent constructor function and assign values to it from data, link it to html div display so each time you make entry it will get displayed in seperate div     
+                          if (jobTitle === "Engineer")
+                          { 
+                            options(jobTitle);
+                          } 
+                          else if (jobTitle === "Intern")
+                          { 
+                            options(jobTitle);
+                          }
+                          else {
+                            console.log("We are all done, thanks !!!")
+                          return 
+                          };
+
+                    //Here you will insert an inherited child function and assign values to it from data, link it to html div display so each time you make entry it will get displayed in seperate div
+
+                  })
+                } 
+                  //this could extend with .then as above for manager 
+        else if (jobTitle === "Intern"){
+          inquirer.prompt(
+            [{
+                type: "input",
+                name: "name",
+                message: "What is the name of the intern?"
+              },  
+              
+              {
+                  type: "input",
+                  name: "id",
+                  message: "What is the employee ID of the intern?"
+                },
+                {
+                  type: "input",
+                  name: "email",
+                  message: "What is the email address of the intern?"
+                  },
+                  {
+                      type: "input",
+                      name: "school",
+                      message: "What is the name of the school for intern?" 
+                    },
+                    { 
+                      type: "list",
+                      name: "continueTitle",
+                      message: "Would you like to continue to add an engineer or an intern to finish building the team(Press <enter> to select)",
+                      choices: ["Engineer", "Intern", "I am done adding team members"]     
+                         //icon will display along with designation         
+                      }
+
+                  ]).then(data =>  {
+                    console.log(data);
+                    jobTitle = data.continueTitle;
+                    //Here you will insert a parent constructor function and assign values to it from data, link it to html div display so each time you make entry it will get displayed in seperate div     
+                          if (jobTitle === "Engineer")
+                          { 
+                            options(jobTitle);
+                          } 
+                          else if (jobTitle === "Intern")
+                          { 
+                            options(jobTitle);
+                          }
+                          else {
+                            console.log("We are all done, thanks !!!")
+                          return 
+                          };
+              //Here you will insert an inherited child function and assign values to it from data, link it to html div display so each time you make entry it will get displayed in seperate div
+
+                  })
+                  
+                } 
+        // you can add .then from here if needed as above          
+      else { 
+        console.log("We are all done, thanks !!!")
+        return 
+      }
+      
+      
+      }
+                //the above curly bracket belongs to function options ending 
+        options(jobTitle);
+    
+  //the following two are ending for .then function  
+  })
+};
+// the above yellow bracket is end of function questions 
+questions();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Acceptance Criteria
@@ -25,9 +225,16 @@
 //WHEN I click on the GitHub username
 //THEN that GitHub profile opens in a new tab
 //WHEN I start the application
-//THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
+
+
+
+
 //WHEN I enter the team manager’s name, employee ID, email address, and office number
 //THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+
+//here
+
 //WHEN I select the engineer option
 //THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
 //WHEN I select the intern option
@@ -82,3 +289,10 @@
 //A sample HTML file generated using your application.
 
 //The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+
+//Hilary assignment:
+
+//once select engineer, another set of questions when done will you like another or exit. Asking for schools instead be asking of GitHub, also video from Hilary has a sample 
+
+
