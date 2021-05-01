ac38bda9a2bc24b311912479773dae67bc1d3385
Johnny <jaani.matharu@gmail.com>
2021-04-30 23:41:49 -0400

    its not sure what the functions are doing, classes are working now

--- index.js
+++ index.js
@@ -73,8 +73,9 @@ const questions = () => {
           
 //follow structure from previous assgn        
 ]).then(data =>  {
- new Employee (data.name); 
- console.log(Manager());
+  let newManager = new Manager(data.name, data.id, data.email, data.designation, data.number);
+ 
+ console.log(newManager, "hello this is the console log i'm looking for ");
  //area needs fixing based on instructions and notepad before moving another section data must work
     let jobTitle = data.designation;
 //Here you will insert a parent constructor function and assign values to it from data, link it to html div display so each time you make entry it will get displayed in seperate div
